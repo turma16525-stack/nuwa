b0331020e56097ebafc123bb3c69d1ebd1f22070
turma16525-stack <turma16525@gmail.com>
2026-02-24 08:40:36 -0300

    Add files via upload

---
+++ products/script.js
@@ -0,0 +1,33 @@
+//document.addEventListener("DOMContentLoaded", () => {
+  //const verMaisBtn = document.getElementById("verMaisBtn");
+  //const colecao = document.getElementById("colecao");
+
+  //verMaisBtn.addEventListener("click", () => {
+   // colecao.scrollIntoView({ behavior: "smooth" });
+//  });
+//});
+
+ // JavaScript para scroll suave nos links de navegação
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                const targetId = this.getAttribute('href').substring(1);
+                const targetElement = document.getElementById(targetId);
+                if (targetElement) {
+                    targetElement.scrollIntoView({
+                        behavior: 'smooth'
+                    });
+                }
+            });
+        });
+
+document.querySelectorAll('header a').forEach(link => {
+  const linkPath = new URL(link.href, window.location.origin).pathname;
+  const currentPath = window.location.pathname;
+
+  if (linkPath === currentPath) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+    });
+  }
+});
